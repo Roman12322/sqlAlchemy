30d7e2c7810878ceb8a89a905ed55b98425a78e0
Roman Heen <mega.fo@list.ru>
2022-12-27 18:43:46 +0700

    Обновлен метод удаления русских пользователей Добавлен метод изменения названия страны Russia на Russian Federation

--- Main_scripts.py
+++ Main_scripts.py
@@ -3,16 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base() #Создание базового класса
-
 class Users_dns(Base):
     __tablename__ = 'Users_dns'
-
+    # Create columns
     user_id = Column("id", Integer, primary_key=True, nullable=False, autoincrement=True)
     username = Column("username", String(50), nullable=False)
     gender = Column("gender", String(1), nullable=False)
     country = Column("country", String(50), nullable=False)
     city = Column("city", String(100), nullable=False)
-
+    # Table initializing
     def __init__(self, username, gender, country, city):
         self.username = username
         self.gender = gender
@@ -45,12 +44,18 @@ def insert_user(username, gender, country, city):
         return 0
 
 def get_all_users():
+    """
+    Получение всех пользователей списком
+    """
     Session = sessionmaker(bind=engine)
     session = Session()
     results = session.query(Users_dns).all()
     return results
 
 def get_usa_users():
+    """
+        Получение всех пользователей списком из США
+        """
     Session = sessionmaker(bind=engine)
     session = Session()
     results = session.query(Users_dns)\
@@ -62,14 +67,33 @@ def get_usa_users():
     return usa_users
 
 def delete_rus_users():
+    """
+    Удаление всех пользователей из Russia
+    """
     try:
-        Session = sessionmaker()
+        Session = sessionmaker(bind=engine)
         session = Session()
-        rus_users = session.query(Users_dns)\
-            .filter(Users_dns.country == 'Russia')
-        session.delete(rus_users)
+        session.query(Users_dns).filter_by(country='Russia').delete()
         session.commit()
-        print("Executed correctly: answer 1")
+        return 1
     except:
-        print("Executed incorrectly: answer 0")
+        return 0
+
+def specify_russia_name():
+    """
+    Изменяет Russia -> Russian Federation
+    """
+    try:
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.query(Users_dns).filter_by(country='Russia').update({Users_dns.country: 'Russian Federation'})
+        session.commit()
+        return 1
+    except:
+        return 0
+
+insert_user('Roman', 'm', 'Russia', 'Moscow')
+specify_russia_name()
+print(get_all_users())
+
 
